794cdbe71238e49fc0a42889fe4a75790af0e062
Oscar Giovanny Cardenas Herrera <oscar.cardenas@tsicol.com>
2024-10-18 11:53:31 -0500

    inicio proyecto 18/10/2024

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+//import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import "./App.css";
 import logo from "./assets/LOGO.png";
 
